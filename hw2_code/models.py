8f86c709d8861943c3174bc3e99388019763d83d
Spencer R Loggia <spencer.loggia98@gmail.com>
2019-10-06 13:57:18 -0400

    working regression tree

--- hw2_code/models.py
+++ hw2_code/models.py
@@ -7,12 +7,8 @@ This file is where you will write most of your code!
 import numpy as np
 
 class RegressionTree(object):
-    def __init__(self, nfeatures, max_depth, usedfeatures=np.zeros(0)):
+    def __init__(self, nfeatures, max_depth):
         self.num_input_features = nfeatures
-        if len(usedfeatures) > 0:
-            self.used_features = usedfeatures
-        else:
-            self.used_features = np.zeros(nfeatures, dtype=np.float)
         self.max_depth = max_depth
         self.feature = -1
         self.theta = -1
@@ -20,83 +16,92 @@ class RegressionTree(object):
         self.left = -1
 
     def scoreSplit(self, feature, threshold, X, y):
-        score = 3
-        return score
+        featureCol = np.transpose(X)[feature]
+        if type(threshold) != np.float64:
+            print('bad')
+        R = np.argwhere(featureCol > threshold)
+        Rmean = np.mean(featureCol[R])
+        L = np.argwhere(featureCol <= threshold)
+        Lmean = np.mean(featureCol[L])
+        ssR = 0
+        ssL = 0
+        for r in R:
+            ssR += np.square(y[r[0]] - Rmean)
+        for l in L:
+            ssL += np.square(y[l[0]] - Lmean)
+        return {'score': ssR + ssL,
+                'left': L,
+                'right': R}
 
     def bestSplit(self, X, y):
         scores = []
-        max_score = -1
-        max_theta = -1
+        min_score = 999999999999999
+        min_theta = 999999999999999
         feature_split = -1
+        minR = []
+        minL = []
         for f in range(0, self.num_input_features):
-            if self.used_features[f] == 0:
-                fmax_score = -1
-                fmax_theta = -1
-                for t in X:
-                    cur_score = self.scoreSplit(f, t[f], X, y)
-                    if cur_score > fmax_score:
-                        fmax_score = cur_score
-                        fmax_theta = t[f]
-                if fmax_score > max_score:
-                    max_score = fmax_score
-                    max_theta = fmax_theta
+            for t in X:
+                if type(t[f]) != np.float64:
+                    print('vbad')
+                cur_score = self.scoreSplit(f, t[f], X, y)
+                if cur_score['score'] < min_score:
+                    min_score = cur_score['score']
+                    min_theta = t[f]
                     feature_split = f
-        return [feature_split, max_theta]
+                    minR = cur_score['right']
+                    minL = cur_score['left']
+        self.feature = feature_split
+        self.theta = min_theta
+        return [minL, minR]
 
     def split(self, X, y):
-        stats = self.bestSplit(X, y)
-        rightX = []
-        righty = []
-        leftX = []
-        lefty = []
-        self.theta = stats[1]
-        feature = stats[0]
-        temp = np.transpose(X)
-        featureCol = temp[self.feature]
-        for i in range(0, len(featureCol)):
-            if featureCol[i] <= self.theta:
-                leftX.append(X[i])
-                lefty.append(y[i])
-            else:
-                rightX.append(X[i])
-                righty.append(y[i])
-
+        data = self.bestSplit(X, y)
+        LX = []
+        Ly = []
+        RX = []
+        Ry = []
+        for i in data[0]:
+            LX.append(X[i[0]])
+            Ly.append(y[i[0]])
+        for i in data[1]:
+            RX.append(X[i[0]])
+            Ry.append(y[i[0]])
                 #use dictionary
-        return {'leftX': leftX,
-                'lefty': lefty,
-                'rightX': rightX,
-                'righty': righty}
+        return {'leftX': np.array(LX),
+                'lefty': np.array(Ly),
+                'rightX': np.array(RX),
+                'righty': np.array(Ry)}
 
-    def fit(self, *, X, y):
+    def fit(self, *, X, y, base=True):
         """ Fit the model.
                    Args:
                    X: A of floats with shape [num_examples, num_features].
                    y: An array of floats with shape [num_examples].
                    max_depth: An int representing the maximum depth of the tree
         """
-        self.X = np.array(X)
-        self.y = np.array(y)
-
-        if self.max_depth > 0:
-            newData = self.split(X, y)
-            self.left = RegressionTree(self.num_input_features, self.max_depth - 1, usedfeatures=self.used_features)
+        newData = self.split(X, y)
+        if self.max_depth > 0 and len(X) > 1:
+            self.left = RegressionTree(self.num_input_features, self.max_depth - 1)
             self.left.fit(X=newData['leftX'], y=newData['lefty'])
-            self.right = RegressionTree(self.num_input_features, self.max_depth - 1, usedfeatures=self.used_features)
+            self.right = RegressionTree(self.num_input_features, self.max_depth - 1)
             self.right.fit(X=newData['rightX'], y=newData['righty'])
             return self
         else:
-            return -1
+            self.left = np.mean(newData['lefty'])
+            self.right = np.mean(newData['righty'])
 
     def predict(self, X):
-        """ Predict.
-        Args:
-                X: A  matrix of floats with shape [num_examples, num_features].
-
-        Returns:
-                An array of floats with shape [num_examples].
-        """
-        # TODO: Implement this!
-        raise Exception("You must implement this method!")
+        if X[self.feature] <= self.theta:
+            if type(self.left) == np.float64:
+                return self.left
+            else:
+                return self.left.predict(X)
+        elif X[self.feature] > self.theta:
+            if type(self.right) == np.float64:
+                return self.right
+            else:
+                return self.right.predict(X)
 
 
 
--- hw2_code/test.py
+++ hw2_code/test.py
@@ -2,8 +2,16 @@ import numpy as np
 import models
 from data import load_data
 
-y = [4,2,6,1,2]
-X = [[4,1,5,4,1], [4,1,4,6,4], [1,4,7,2,3], [4,6,7,2,5]]
+y = [4,2,6,1,2,3]
+X = [[4,1,5,4,1], [4,1,4,6,4], [4,1,1,6,5], [1,4,7,2,3], [4,6,7,2,5], [1,1,1,2,6]]
+X, y = load_data('../data/train.txt')
 
-test = models.RegressionTree(nfeatures=4, max_depth=2)
+test = models.RegressionTree(nfeatures=5, max_depth=5)
 test = test.fit(X=X, y=y)
+# pred = test.predict(X[2])
+
+devX, devy = load_data('../data/train.txt')
+pred = test.predict(devX[5])
+print("pred: " + str(pred))
+print("val: " + str(devy[2]))
+
